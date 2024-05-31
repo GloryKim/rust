fff1b56f9b239368d76343d781f6751d811baea9
GloryKim <yklovejesus@gmail.com>
2024-05-31 17:59:15 +0900

    240531_1758_glory

---
+++ 240531_1757_hello-world/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+//240531_1758_glory : cargo run 치면 컴파일 됩니다.
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rust
 Linux Kernel, Tokio, Actix-web, Rocket
+
+240531_1758_glory : hello world
